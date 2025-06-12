3ee46c4f1a41610df0c7c0837a7a20669395cb70
nghgtu <tecktrekking@gmail.com>
2025-06-12 09:11:10 +0700

    initial commit

---
+++ using-while-loop/bai01/app.js
@@ -0,0 +1,8 @@
+let num = +prompt("Enter a number: ");
+let total = 0;
+while( num != -1 ) {
+   num = prompt("Enter a number: ");
+   alert(number);
+   total += num;
+} 
+alert("Tổng các số vừa nhập là: " + total);
