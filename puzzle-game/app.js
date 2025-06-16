a4a8d3094258c08e0269d536ba1970153a87dcfe
nghgtu <tecktrekking@gmail.com>
2025-06-16 10:24:41 +0700

    update commit

--- puzzle-game/app.js
+++ puzzle-game/app.js
@@ -6,17 +6,16 @@ const img5 = "images/random_img_5.jpg";
 
 document.getElementById("_container").addEventListener("click", check = () => {
     // const images = ["", img1, img2, img3, img4, img5];
-    let correct = false;
+    let counter = 0;
     let swapSrc1 = document.getElementById("imgswap1").src;
     for(let i = 2; i <= 5; i++) {
         const imgsrc = document.getElementById(`imgswap${i}`).src;
-        if (imgsrc === swapSrc1 && i === 5) {
-            correct = true;
-            break;
+        if (imgsrc === swapSrc1) {
+            counter++;
         }
     }
 
-    if (correct) {
+    if (counter === 4) {
         // document.getElementById("result").innerHTML = "Correct!";
         // document.getElementById("result").style.color = "green";
         alert("Congratulations! You solved the puzzle!");
