b4c06df8ff7a5af085361104ddf2717f56766cfb
nghgtu <tecktrekking@gmail.com>
2025-06-24 22:41:14 +0700

    update

--- oop2/tic-tac-toe2/app.js
+++ oop2/tic-tac-toe2/app.js
@@ -1,156 +1,32 @@
 var turn = 0;
-
 let b = document.getElementById("tic-tac-toe");
-
-let board = [];
+let cells = [];
 let data = "";
-// for (let i = 0; i < 5; i++) {
-//     board[i] = new Array(0, 0, 0, 0, 0);
-// }
 
-// for (let i = 0; i < 5; i++) {
-//     data += "<br/>";
-//     for (let j = 0; j < 5; j++) {
-//         board[i][j] = "(.)";
-//         data += board[i][j] + "&nbsp;&nbsp;";
-//     }
-// }
-
-// b.innerHTML = data;
-
-// function checkAdjacent(i, j) {  
-//     if (i < 0 || i >= 5 || j < 0 || j >= 5) {
-//         return false;
-//     }
-//     return board[i][j] !== "(.)";
-// }
-
-// function checkWinner(player, x, y) {
-//     // horizontal
-//     if (board[x][y-2] === player && board[x][y-1] === player) {
-//         alert(`Nguoi choi ${player} da thang!!!`);
-//             return;
-//     }
-//     if (board[x][y-1] === player && board[x][y+1] === player)  {
-//         alert(`Nguoi choi ${player} da thang!!!`);
-//             return;
-//     }
-//     if (board[x][y+1] === player && board[x][y+2] === player)  {
-//         alert(`Nguoi choi ${player} da thang!!!`);
-//             return;
-//     }
-    
-//     // vertical
-//     if (board[x-2][y] === player && board[x-1][y] === player)  {
-//         alert(`Nguoi choi ${player} da thang!!!`);
-//             return;
-//     }
-//     if (board[x-1][y] === player && board[x+1][y] === player)  {
-//         alert(`Nguoi choi ${player} da thang!!!`);
-//             return;
-//     }
-//     if (board[x+1][y] === player && board[x+2][y] === player)  {
-//         alert(`Nguoi choi ${player} da thang!!!`);
-//             return;
-//     }
-
-//     // diagonal 
-//     if (board[x-2][y-2] === player && board[x-1][y-1] === player)  {
-//         alert(`Nguoi choi ${player} da thang!!!`);
-//             return;
-//     }
-//     if (board[x-1][y+1] === player && board[x+1][y-1] === player)  {
-//         alert(`Nguoi choi ${player} da thang!!!`);
-//             return;
-//     }
-//     if (board[x-2][y+2] === player && board[x-1][y+1] === player)  {
-//         alert(`Nguoi choi ${player} da thang!!!`);
-//             return;
-//     }
-//     if (board[x+2][y-2] === player && board[x+1][y-1] === player)  {
-//         alert(`Nguoi choi ${player} da thang!!!`);
-//             return;
-//     }
-//     if (board[x-1][y-1] === player && board[x+1][y+1] === player)  {
-//         alert(`Nguoi choi ${player} da thang!!!`);
-//             return;
-//     }
-//     if (board[x+1][y+1] === player && board[x+2][y+2] === player)  {
-//         alert(`Nguoi choi ${player} da thang!!!`);
-//             return;
-//     }
-// }
-
-// function changeValue() {
-//     if (turn === 0) {
-//         let positionX, positionY;
-//         do {
-//             positionX = +prompt("Player 1 move X: ");
-//             positionY = +prompt("Player 1 move Y: ");
-//             if (board[positionX][positionY] !== "(.)") {
-//                 alert("Vi tri ${positionX} , ${positionY} ko hop le. Vui long nhap lai.");
-//                 continue;
-//             }
-//             break;
-//         }
-//         while (true);
-
-//         data = "";
-//         board[positionX][positionY] = "o";
-//         for (let i = 0; i < 5; i++) {
-//             data += "<br/>";
-//             for (let j = 0; j < 5; j++) {
-//                 data += board[i][j] + "&nbsp;&nbsp;&nbsp;&nbsp;";
-//             }
-//         }
-//         b.innerHTML = "<hr/>" + data;
-//         turn = 1;
-//         checkWinner("o", positionX, positionY);
-//         } else {
-//             let positionX, positionY;
-//             do {
-//                 positionX = +prompt("Player 1 move X: ");
-//                 positionY = +prompt("Player 1 move Y: ");
-//                 if (board[positionX][positionY] !== "(.)") {
-//                     alert("Vi tri ${positionX} , ${positionY} ko hop le. Vui long nhap lai.");
-//                     continue;
-//                 }
-//                 break;
-//             }
-//             while (true);
-//                 data = "";
-//                 board[positionX][positionY] = "x";
-//             for (let i = 0; i < 5; i++) {
-//                 data += "<br/>";
-//                 for (let j = 0; j < 5; j++) {
-//                     data += board[i][j] + "&nbsp;&nbsp;&nbsp;&nbsp;";
-//                 }
-//             }
-//             b.innerHTML = "<hr/>" + data;
-//             turn = 0;
-//             checkWinner("x", positionX, positionY);
-//     }
-// }
-
-class Cell{
-    constructor(x=0, y=0, value="") {
-        this.x = x;
-        this.y = y;
-        this.value = value;
+function reset() {
+    b.innerHTML = "";
+    data = "";
+    for (let i = 0; i < 20; i++) {
+    data += `<tr>`;
+    cells[i] = [];
+    for (let j = 0; j < 20; j++) {
+        data += `
+            <td
+                /* id="${i}-${j}" */
+                onclick="
+                mark(this);
+                play(${i},${j});
+                "
+            ></td>
+            `;
+        cells[i][j] = null;
+        }
+        data += '</tr>';
     }
 
-    checkWinner() {
-        let i = this.x;
-        let j = this.y;
-        let count = 1;
-        // horizontal
-        
-    }
+b.innerHTML += data;
+
 }
-
-// function checkWinner(i, j) {
-    
-// }
 
 function mark(e) {
     if (e.innerHTML !== "") {
@@ -159,28 +35,65 @@ function mark(e) {
     }  
     if (turn === 0) {
         e.innerHTML = '<span style="color: blue">X</span>';
-        turn = 1;
     } else {
         e.innerHTML = '<span style="color: red">O</span>';
-        turn = 0;
     }
+}
+
+
+function checkWinner(x, y) {
+    const player = cells[x][y];
+    if (player === null) return;
+
+    let count = 1;
     
-}
-
-for (let i = 1; i <= 20; i++) {
-    data += `<tr>`;
-    for (let j = 1; j <= 20; j++) {
-        data += `
-            <td
-                id="${i}-${j}"
-                onclick="
-                mark(this);
-
-                "
-            ></td>
-            `;
+    // horizontal
+    // leftwards
+    for (let j = y - 1; j >= 0 && cells[x][j] === player; j--) count++;
+    // rightwards
+    for (let j = y + 1; j < 20 && cells[x][j] === player; j++) count++;
+    if (count == 5){
+        turn == 1 ? alert('Player 2 won!') : alert('Player 1 won!');
+        setTimeout(reset, 500);
     }
-    data += '</tr>';
+    count = 1;
+
+    // vertical
+    // upwards
+    for (let i = x - 1; i >= 0 && cells[i][y] === player; i--) count++;
+    // downrwards
+    for (let i = x + 1; x < 20 && cells[i][y] === player; i++) count++;
+    if (count == 5){
+        turn == 1 ? alert('Player 2 won!') : alert('Player 1 won!');
+        setTimeout(reset, 500);
+    }
+    count = 1;
+
+    // diagonal
+    // top-left to bottom-right
+    for(let i = x-1, j = y-1; i >= 0 && j >= 0 && cells[i][j] === player; i--, j--) count++;
+    for(let i = x+1, j = y+1; i < 20 && j < 20 && cells[i][j] === player; i++, j++) count++;
+    if (count == 5){
+        turn == 1 ? alert('Player 2 won!') : alert('Player 1 won!');
+        setTimeout(reset, 500);
+    }
+    count = 1;
+
+    // top-right to bottom-left
+    for(let i = x-1, j = y+1; i >= 0 && j < 20 && cells[i][j] === player; i--, j++) count++;
+    for(let i = x+1, j = y-1; i < 20 && j >= 0 && cells[i][j] === player; i++, j--) count++;
+    if (count == 5){
+        turn == 1 ? alert('Player 2 won!') : alert('Player 1 won!');
+        setTimeout(reset, 500);
+    }
 }
 
-b.innerHTML += data;
+function play(i, j) {
+    if (cells[i][j] !== null) return;
+
+    cells[i][j] = turn;
+    checkWinner(i, j);
+    turn = 1 - turn;
+}
+
+window.onload = reset();
